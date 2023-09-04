645464aaf535161aca1553e62ae0b920fc03bd88
AnaMaria <ana.accilio@utec.edu.pe>
2023-09-04 18:15:22 -0500

    avl rotation_left and right

--- avl.h
+++ avl.h
@@ -39,34 +39,37 @@ struct Record
 
 };
 
-class AVLFile
-{
+class AVLFile {
 private:
     bool primary_key = true;
     string filename;
-    long pos_root;
+    long pos_node;
 public:
-    AVLFile(string filename){
-        this->pos_root = 0;
+    AVLFile(string filename) {
+        this->pos_node = 0;
         this->filename = filename;
     }
-    Record find(int key){
-        return find(pos_root, key);
+
+    Record find(int key) {
+        return find(pos_node, key);
     }
-    void insert(Record record){
-        insert(pos_root, record);
+
+    void insert(Record record) {
+        insert(pos_node, record);
     }
+    /*
     vector<Record> inorder(){
         return inorder(pos_root);
     }
+     */
 private:
 
     //altura de los nodos
-    long height(long pos_node){
+    long height(long pos_node) {
         ifstream file(filename, ios::binary);
         Record record;
         file.seekg(pos_node, ios::beg);
-        file.read((char*)&record, sizeof(Record));
+        file.read((char *) &record, sizeof(Record));
         file.close();
         if (pos_node == -1) {
             return -1;
@@ -77,45 +80,46 @@ private:
 
     // factor de balanceo
 
-    int balancing_factor(long pos_node){
+    int balancing_factor(long pos_node) {
         ifstream file(filename, ios::binary);
         Record record;
         file.seekg(pos_node, ios::beg);
-        file.read((char*)&record, sizeof(Record));
+        file.read((char *) &record, sizeof(Record));
         file.close();
         if (pos_node == -1) {
             return 0;
         } else {
             return height(record.right) - height(record.left);
         }
+
     }
 
+
     // actualización de la altura por cada nodo
-    void update_height(long pos_node){
+    void update_height(long pos_node) {
         ifstream file(filename, ios::binary);
         Record record;
         file.seekg(pos_node, ios::beg);
-        file.read((char*)&record, sizeof(Record));
+        file.read((char *) &record, sizeof(Record));
         file.close();
         if (pos_node != -1) {
             record.height = 1 + max(height(record.left), height(record.right));
             fstream file(filename, ios::in | ios::out | ios::binary);
             file.seekp(pos_node, ios::beg);
-            file.write((char*)&record, sizeof(Record));
+            file.write((char *) &record, sizeof(Record));
             file.close();
         }
     }
 
 
-
     //  find
 
 
-    Record find(long pos_node, int key){
+    Record find(long pos_node, int key) {
         ifstream file(filename, ios::binary);
         Record record;
         file.seekg(pos_node, ios::beg);
-        file.read((char*)&record, sizeof(Record));
+        file.read((char *) &record, sizeof(Record));
 
         if (record.cod == key) {
             return record;
@@ -142,58 +146,53 @@ private:
 
     //insert
 
-    void insert(long pos_node, Record record){
+    void insert(long pos_node, Record record) {
         fstream f(this->filename, ios::app | ios::in | ios::out | ios::binary);
 
-        if(f.tellg() == 0){
-            f.write((char*)&record, sizeof(record));
+        if (f.tellg() == 0) {
+            f.write((char *) &record, sizeof(record));
             f.close();
             return;
-        }
-
-
-        else{
+        } else {
             Record curr_record;
             long curr_pos = pos_node;
 
-            while(true){
+            while (true) {
                 f.seekg(curr_pos, ios::beg);
-                f.read((char*)&curr_record, sizeof(Record));
+                f.read((char *) &curr_record, sizeof(Record));
 
-                if(record.cod > curr_record.cod){
-                    if(curr_record.right == -1){
+                if (record.cod > curr_record.cod) {
+                    if (curr_record.right == -1) {
                         f.seekg(0, ios::end);
                         curr_record.right = f.tellg();
-                        f.write((char*)&record, sizeof(Record));
+                        f.write((char *) &record, sizeof(Record));
                         f.close();
 
                         fstream file(this->filename, ios::in | ios::out | ios::binary);
                         file.seekp(curr_pos, ios::beg);
-                        file.write((char*)&curr_record, sizeof(Record));
+                        file.write((char *) &curr_record, sizeof(Record));
                         file.close();
                         return;
-                    }
-                    else{
+                    } else {
                         curr_pos = curr_record.right;
                     }
-                }else if(record.cod < curr_record.cod){
-                    if(curr_record.left == -1){
+                } else if (record.cod < curr_record.cod) {
+                    if (curr_record.left == -1) {
                         f.seekg(0, ios::end);
                         curr_record.left = f.tellg();
-                        f.write((char*)&record, sizeof(Record));
+                        f.write((char *) &record, sizeof(Record));
                         f.close();
 
                         fstream file(this->filename, ios::in | ios::out | ios::binary);
                         file.seekp(curr_pos, ios::beg);
-                        file.write((char*)&curr_record, sizeof(Record));
+                        file.write((char *) &curr_record, sizeof(Record));
                         file.close();
 
                         return;
-                    }
-                    else{
+                    } else {
                         curr_pos = curr_record.left;
                     }
-                }else{
+                } else {
                     f.close();
                     return;
                 }
@@ -201,44 +200,157 @@ private:
 
         }
     }
-    /*
 
-    vector<Record> inorder(long pos_node){
-        vector<Record> result;
 
-        if (pos_node != -1) {
+    long find_max(long left) {
+        if (left != -1) {
             ifstream file(filename, ios::binary);
             Record record;
-            file.seekg(pos_node, ios::beg);
-            file.read((char*)&record, sizeof(Record));
-
-            vector<Record> leftSubtree = inorder(record.left);
-            result.insert(result.end(), leftSubtree.begin(), leftSubtree.end());
-
-            result.push_back(record);
-
-            vector<Record> rightSubtree = inorder(record.right);
-            result.insert(result.end(), rightSubtree.begin(), rightSubtree.end());
-
+            file.seekg(left, ios::beg);
+            file.read((char *) &record, sizeof(Record));
             file.close();
+            if (record.right != -1) {
+                return find_max(record.right);
+            } else {
+                return left;
+            }
         }
-
-        return result;
     }
-     */
+
+    long find_min(long right) {
+        if (right != -1) {
+            ifstream file(filename, ios::binary);
+            Record record;
+            file.seekg(right, ios::beg);
+            file.read((char *) &record, sizeof(Record));
+            file.close();
+            if (record.left != -1) {
+                return find_min(record.left);
+            } else {
+                return right;
+            }
+        }
+    }
+
 
     // rotación a la derecha
-    void right_rotation(long &pos_node){
+    void right_rotation(long &pos_node) {
+        ifstream file(filename, ios::binary);
+        Record record;
+        file.seekg(pos_node, ios::beg);
+        file.read((char *) &record, sizeof(Record));
+        file.close();
+        if (pos_node != -1) {
+            long aux = record.left;
+            record.left = find_max(record.left);
+            update_height(record.left);
+            fstream file(filename, ios::in | ios::out | ios::binary);
+            file.seekp(pos_node, ios::beg);
+            file.write((char *) &record, sizeof(Record));
+            file.close();
+            right_rotation(aux);
+        } else if (pos_node == -10) {
+            return;
+        }
+
     }
 
-    // rotación a la izquierda
-    void left_rotation(long &pos_node){
+
+// rotación a la izquierda
+    void left_rotation(long &pos_node) {
+        ifstream file(filename, ios::binary);
+        Record record;
+        file.seekg(pos_node, ios::beg);
+        file.read((char *) &record, sizeof(Record));
+        file.close();
+        if (pos_node != -1) {
+            long aux = record.right;
+            record.right = find_min(record.right);
+            update_height(record.right);
+            record.left = aux;
+            update_height(record.left);
+            pos_node = record.right;
+            update_height(pos_node);
+        } else if (pos_node == -1) {
+            return;
+        }
     }
+
 
     // balanceo
 
     void balance(long &pos_node){
+        ifstream file(filename, ios::binary);
+        Record record;
+        file.seekg(pos_node, ios::beg);
+        file.read((char*)&record, sizeof(Record));
+        file.close();
+        if(pos_node != -1){
+            int bf = balancing_factor(pos_node);
+            if(bf > 1){
+                if(balancing_factor(record.right) < 0){
+                    right_rotation(record.right);
+                }
+                left_rotation(pos_node);
+            }
+            else if(bf < -1){
+                if(balancing_factor(record.left) > 0){
+                    left_rotation(record.left);
+                }
+                right_rotation(pos_node);
+            }
+            else{
+                update_height(pos_node);
+            }
+        }
+        else if(pos_node == -1){
+            return;
+        }
     }
+
+    void remove(long &pos_node, int cod){
+        ifstream file(filename, ios::binary);
+        Record record;
+        file.seekg(pos_node, ios::beg);
+        file.read((char*)&record, sizeof(Record));
+        file.close();
+        if(pos_node != -1){
+            if(cod < record.cod){
+                remove(record.left, cod);
+            }
+            else if(cod > record.cod){
+                remove(record.right, cod);
+            }
+            else{
+                if(record.left == -1 && record.right == -1){
+                    pos_node = -1;
+                }
+                else if(record.left == -1){
+                    pos_node = record.right;
+                }
+                else if(record.right == -1){
+                    pos_node = record.left;
+                }
+                else{
+                    long aux = record.left;
+                    record.left = find_max(record.left);
+                    update_height(record.left);
+                    fstream file(filename, ios::in | ios::out | ios::binary);
+                    file.seekp(pos_node, ios::beg);
+                    file.write((char*)&record, sizeof(Record));
+                    file.close();
+                    right_rotation(aux);
+                }
+            }
+            balance(pos_node);
+        }
+        else if(pos_node == -1){
+            return;
+        }
+    }
+
+
+
 
 };
 
@@ -252,6 +364,7 @@ void writeFile(string filename){
     }
 }
 
+/*
 void readFile(string filename){
     AVLFile file(filename);
     cout<<"--------- show all sorted data -----------\n";
@@ -260,10 +373,10 @@ void readFile(string filename){
         re.showData();
     }
 }
-
+*/
 int main() {
     writeFile("data.bin");
-    readFile("data.bin");
+    //readFile("data.bin");
     return 0;
 }
 
